1cdce2488a0aab7b9619e69e237e1b2aa70b5dda
Alex <alex.van.bet@gmail.com>
2019-10-03 13:50:59 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.number = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.number = this.min + Math.round((this.max - this.min) / 2)
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
